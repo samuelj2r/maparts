f632473ee4609cb02b29c48f7b6d7186ddfdda33
samuel <samuelj2r@gmail.com>
2024-10-26 18:42:38 +0200

    binding ports for production

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-});
+  server: {
+    host: mode === 'production' ? '0.0.0.0' : 'localhost',
+    port: 5173,
+  },
+}));
