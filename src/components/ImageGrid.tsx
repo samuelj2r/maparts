3a989c923a79c0c915c30f9e76ea364cdfb25d95
samuel <samuelj2r@gmail.com>
2024-10-26 20:05:07 +0200

    change to 1 column for mobile

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -50,7 +50,7 @@ const ImageGrid = ({ images, loading, deviceType }: ImageGridProps) => {
 
   const gridClass = deviceType === 'desktop'
     ? 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3'
-    : 'grid-cols-2 md:grid-cols-3 lg:grid-cols-3';
+    : 'grid-cols-1 md:grid-cols-3 lg:grid-cols-3';
 
   if (loading) {
     return (
